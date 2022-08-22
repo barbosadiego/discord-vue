0b15bc45514245034dc19c38c25f6851cf4df656
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-22 16:19:29 -0300

    chat-area component

---
+++ src/components/ChatArea.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="chat-area">
+    ChatArea
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'ChatArea',
+}
+</script>
+
+<style lang="scss" scoped>
+.chat-area{
+  grid-row: 2/4;
+  grid-column: 3/4;
+  width: 100%;
+  background-color: var(--bg-color-light);
+}
+</style>
