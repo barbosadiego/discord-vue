9514bfbad3fafa3e9b3ad66f247f711819e924c1
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-23 16:01:56 -0300

    user-list component created

---
+++ src/components/UserList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="user-list">
+    <div class="online">
+      <span>DISPONÍVEL - 1</span>
+      
+    </div>
+    <div class="offline">
+      <span>OFFLINE - 18</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+}
+</script>
+
+<style lang="scss" scoped>
+.user-list{
+  grid-column: 4/5;
+  grid-row: 2/4;
+  background-color: var(--bg-color-medium);
+}
+</style>
