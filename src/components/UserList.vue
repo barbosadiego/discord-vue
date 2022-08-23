76e72dffff278ee5a2731f316602825cad635b9c
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-23 16:07:56 -0300

    add avatar components

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -2,17 +2,24 @@
   <div class="user-list">
     <div class="online">
       <span>DISPONÍVEL - 1</span>
-      
+      <Avatar name="Guilherme Rodz"/>
     </div>
     <div class="offline">
       <span>OFFLINE - 18</span>
+      <Avatar name="Diego Fernandes"/>
+      <Avatar name="Fulano" v-for="item in 18" :key="item.id"/>
     </div>
   </div>
 </template>
 
 <script>
+import Avatar from '@/components/Avatar.vue';
+
 export default {
   name: 'UserList',
+  components:{
+    Avatar,
+  }
 }
 </script>
 
